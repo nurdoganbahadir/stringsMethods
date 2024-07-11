50e188643a94653c1dba8378f3d31f52dda182ce
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-11 14:45:32 +0300

    endsWith added

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let metin = "Kocaeli'de yaşıyorum.";
+// let metin = "Kocaeli'de yaşıyorum.";
 
 //? charAt() belirtilen indeks numarasındaki string ifadeyi döndürür.
-let inMetin = metin.charAt(1);
-console.log(inMetin);
+// let inMetin = metin.charAt(1);
+// console.log(inMetin);
 
 //? charCodeAt() unicode karakter kodunu döndürür.
-let inMetin2 = metin.charCodeAt(2);
-console.log(inMetin2);
+// let inMetin2 = metin.charCodeAt(2);
+// console.log(inMetin2);
 
 //? concat() methodu 2 farklı string ifadeyi birleştirmeye yarar.
-let list1 = ["fiat", "renault", "porsche", "citroen", "peugeot"];
-let list2 = ["lambo", "ferrari"];
-let lists = list1.concat(list2);
+// let list1 = ["fiat", "renault", "porsche", "citroen", "peugeot"];
+// let list2 = ["lambo", "ferrari"];
+// let lists = list1.concat(list2);
+// console.log(lists);
+
+//? endsWith() içine aldığı değerin sonda olup olmadığını kontrol eder. Boolen olarak döndürür.
+let list = "Kocaeli'de yaşıyorum.";
+let lists = list.endsWith(".");
 console.log(lists);
+
