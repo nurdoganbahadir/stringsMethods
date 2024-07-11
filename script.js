09f40f5b4b8d4b49cd29868946209201674c3fda
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-11 14:26:12 +0300

    charCodeAt added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let metin = "Kocaeli'de yaşıyorum.";
 
-// charAt()
+//? charAt() belirtilen indeks numarasındaki string ifadeyi döndürür.
 let inMetin = metin.charAt(1);
 console.log(inMetin);
+
+//? charCodeAt() unicode karakter kodunu döndürür.
+let inMetin2 = metin.charCodeAt(2);
+console.log(inMetin2);
+
+//? concat();
